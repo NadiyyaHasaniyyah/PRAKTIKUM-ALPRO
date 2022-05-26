cb2acb70a4da6859401340059891656c397cb6c0
Nadiyya <89526057+NadiyyaHasaniyyah@users.noreply.github.com>
2022-05-26 10:06:05 +0700

    Add files via upload
    Tugas 3 UTS Alpro no 2-3

---
+++ PRAKTIKUM3/nomor2UTS.cpp
@@ -0,0 +1,32 @@
+//Nama File: Nomor 2
+//Pembuat: Nadiyya Hasaniyyah
+//NIM:24060121120030
+//Kelas: B
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Kamus
+	int i,j,N,NBArray;
+
+    //Algoritma
+    printf("Membuat Program Menghitung Array\n");
+    printf("Masukan nilai N : ");
+    scanf("%d", &N);
+    int T[N];
+    NBArray = 0;
+
+    for (i = 0; i <= N; i++) scanf("%d", &T[i]);
+
+    for (i = 0; i <= N; i++) {
+        for (j = i; j < N; j++) {
+            NBArray += T[j];
+        }
+    }
+    printf("%d", NBArray);
+    return 0;
+}
---
+++ PRAKTIKUM3/nomor3UTS.cpp
@@ -0,0 +1,32 @@
+//Nama File: Nomor 3
+//Tanggal: 30 Maret 2022
+//Pembuat: Nadiyya Hasaniyyah
+//NIM:24060121120030
+//Kelas: B
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Kamus
+	int i,N,jarak, Kecil, Besar;
+
+    //Algoritma
+    printf("Selisih Ketinggian \n");
+    scanf("%d", &N);
+    int T[N];
+
+    for (i = 0; i < N; i++) scanf("%d", &T[i]);
+
+    Besar = T[0];
+    Kecil = T[0];
+
+    for (i = 0; i < N; i++) if (T[i] > Besar) Besar = T[i];
+
+    for (i = 0; i < N; i++) if (T[i] < Kecil) Kecil = T[i];
+
+    jarak = Besar-Kecil;
+    printf("%d", jarak);
+    return 0;
+}
